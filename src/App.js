ff37b5129e6775796eb2e39fb619b6add5af1fc2
Veronika <xafling@mail.ru>
2023-04-06 14:47:50 +0300

    Form Login/Logout is ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import StoreApi from './data/store'
+import StoreApi from './data/store';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import './index.css';
@@ -10,10 +10,16 @@ const App = () => {
 
   const [login, setLogin] = useState();
   const [active, setActive] = useState(false);
+  const [error, setError] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    setError(false)
+  }
 
   return (
     <div className='wrapper'>
-       <StoreApi.Provider value={{  login, setLogin
+       <StoreApi.Provider value={{  login, setLogin, onSubmit
         // onSubmit, login, setLogin, email, setEmail, password, setPassword,
         // error, setError, data, setData, personsInfo, setPersonsInfo, cases, setCases
       }}>
--- src/components/Authorization/Authorization.jsx
+++ src/components/Authorization/Authorization.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {Link} from 'react-router-dom'
+import store from '../../data/store'
 import './Authorization.css'
 
 const Authorization = ({active, setActive}) => {
+
+    const {
+        onSubmit, email,
+        changeEmail, password, 
+        changePass, error, 
+        // data, setData,
+        // email, setEmail,
+        // error, setError, 
+        // password, setPassword
+    } = useContext(store)
 
     const activateAuthorization = () => {
         setActive(false)
     }
 
     return(
-        <div className={ active ? 'signIn_window activate' : 'signIn_window' } onClick={activateAuthorization} >
-            <form className='signIn' onSubmit={onSubmit} onClick={stop}>
-                <Link to='/signUp' onClick={activateAuthorization} className='auth'>Registration</Link>
+        <div className={ active ? 'authorization_window activate' : 'authorization_window' } onClick={activateAuthorization} >
+            <form className='authorization' onSubmit={onSubmit} onClick={stop}>
+                <Link to='/authorization' onClick={activateAuthorization} className='auth'>Registration</Link>
                 <input type="text" value={email} onChange={changeEmail} placeholder='Email' />
                 <input type="password" value={password} onChange={changePass} placeholder='Password' />
                 {error && <p className='alarm'>Error</p>}
-                <button type='submit' className='signIn_button'>Sign In</button>
+                <button type='submit' className='authorization_button'>Sign In</button>
             </form>
         </div>
     )
